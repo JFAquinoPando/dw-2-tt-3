d8692210b5ee93d9d613a4641ae65c44b31f2d85
jfaquinopando <pandolcik@gmail.com>
2023-03-11 16:25:31 -0300

    Salvado antes de borrar todo

--- recibir.php
+++ recibir.php
@@ -1,10 +1,10 @@
 <?php
     if($_POST["fecha"] <= "2023-03-11"){
-        echo "<div style='color:{$_POST['colorFav']};
-        background:{$_POST['colorFav2']};'>";
+        echo "<body style='color:{$_POST['colorFav']};
+        background:{$_POST['colorFav2']};'><div >";
         echo "Fecha Valida <br>";
         print_r($_POST);
-        echo "</div>";
+        echo "</div></body>";
     }
     else {
         echo "Fecha invalida, no se permite del futuro";
